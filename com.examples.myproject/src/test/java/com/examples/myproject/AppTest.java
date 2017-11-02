ec6f87e0625c28d04ea1c3dadbb158a193c628bd
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-11-02 16:49:35 +0100

    added missing test

--- com.examples.myproject/src/test/java/com/examples/myproject/AppTest.java
+++ com.examples.myproject/src/test/java/com/examples/myproject/AppTest.java
@@ -20,4 +20,9 @@ public class AppTest {
 	public void testSayHello() {
 		assertEquals("Hello", app.sayHello());
 	}
+	
+	@Test
+	public void testSayHelloWithName() {
+		assertEquals("Hello World", app.sayHello("World"));
+	}
 }
