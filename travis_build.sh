9140a0f7f459a53e38a3a24f0a03513bd274523f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-11-10 19:01:01 +0100

    run jacoco goals explicitly in Travis

--- travis_build.sh
+++ travis_build.sh
@@ -3,7 +3,7 @@
 set -ev
 if [ "$WITH_COVERALLS" == "true" ];
 then
-  mvn -f com.examples.myproject/pom.xml clean verify -Pjacoco coveralls:report;
+  mvn -f com.examples.myproject/pom.xml clean jacoco:prepare-agent verify jacoco:report coveralls:report;
 elif [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   mvn -f com.examples.myproject/pom.xml clean verify;
 fi
